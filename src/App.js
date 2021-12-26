6c99d083aa856c61d2b36cde200ef57dbc33887f
Burak Özkan <burak.ozkan@path.com.tr>
2021-12-26 13:28:35 +0300

    add states for usersign up component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import UserSignUpPage from './components/UserSignUpPage';
 
 function App() {
   return (
     <div className="App">
-     
+     <UserSignUpPage />
     </div>
   );
 }
--- src/components/UserSignUpPage.js
+++ src/components/UserSignUpPage.js
@@ -1,27 +1,49 @@
-import React from 'react'
+import React,{useState} from 'react'
+
+const INIT_STATE = {
+  email: '',
+  password: '',
+  passwordConfirmation: '',
+  displayName: ''
+}
 
 const UserSignUpPage = () => {
+  const [agreedStatus, setAgreedStatus] = useState(false);
+  const [pageState, setPageState] = useState(INIT_STATE);
+
+  const onınputChange = (e) => {
+    const { name, value } = e.target;
+    setPageState({
+      ...pageState,
+      [name]: value
+    });
+  }
+
   return (
     <>
       <form>
         <h1>Sign Up</h1>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
+          <input type="email" name="email" id="email" value={pageState.email} onChange={onınputChange}/>
         </div>
         <div>
           <label htmlFor="d-name">Display Name</label>
-          <input type="text" id="d-name" />
+          <input type="text" name="displayName" id="d-name" value={pageState.displayName} onChange={onınputChange} />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
+          <input type="password" name="password" id="password" value={pageState.password} onChange={onınputChange} />
         </div>
         <div>
-          <label htmlFor="passwordR">Password Repeat</label>
-          <input type="password" id="passwordR" />
+          <label htmlFor="passwordC">Password Repeat</label>
+          <input type="password" name="passwordConfirmation" id="passwordC" value={pageState.passwordConfirmation} onChange={onınputChange} />
         </div>
-        <button type='submit'>Sign Up</button>
+        <div>
+          <label htmlFor="agree">Agreed</label>
+          <input type="checkbox" id="agree" value={agreedStatus} onChange={(e)=> setAgreedStatus(prevState => prevState = !prevState)} />
+        </div>
+        <button disabled={agreedStatus} type='submit'>Sign Up</button>
       </form>
     </>
   )
